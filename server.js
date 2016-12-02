39422b5caa1530de74fe455796abdbba3257641e
Sanjay Parajuli <sanjayparajuli343@gmail.com>
2016-12-02 18:17:15 +1100

    updated

---
+++ server.js
@@ -0,0 +1,11 @@
+// server.js
+
+//BASE SETUP 
+
+
+// call the packages we need
+
+
+var express = require('express'); //call the express
+var app = express();   //define our app using express
+var bodyParser = require('body-parser');
